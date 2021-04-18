a815b796e97daf44c868404f055fd145733292c2
Felipe Freitag <felipefreitag08@hotmail.com>
2021-04-18 18:39:59 -0300

    Add Beginner 1187-1190 in cpp

---
+++ Begginer/1188.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+
+int main(){
+    double M[12][12], answer = 0, num;
+    char operation;
+
+    std::cin >> operation;
+
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            std::cin >> num;
+            M[i][j] = num;
+        }
+    }
+
+    for(int i = 11, y = 1; i > 6; i--, y++){
+        for(int j = y; j < i; j++){
+            answer += M[i][j];
+        }
+    }
+
+    if(operation == 'M'){
+        answer = answer / 30;
+    }
+
+    std::cout << std::fixed << std::setprecision(1) << answer << std::endl;
+
+    return 0;
+}
---
+++ Begginer/1190.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+
+int main(){
+    double M[12][12], num, answer = 0;
+    char operation;
+
+    std::cin >> operation;
+
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            std::cin >> num;
+            M[i][j] = num;
+        }
+    }
+
+    for(int i = 1; i < 6; i++){
+        for(int j = 11; j > 11 - i; j--){
+            answer += M[i][j];
+        }
+    }
+
+    for(int i = 6, y = 6; i < 11; i++, y++){
+        for(int j = 11; j > y; j--){
+            answer += M[i][j];
+        }
+    }
+
+    if(operation == 'M'){
+        answer /= 30;
+    }
+
+    std::cout << std::fixed << std::setprecision(1) << answer << std::endl;
+
+    return 0;
+}
